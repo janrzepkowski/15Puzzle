edd78545719b38ce7b18f749b308b0e4aceab5e6
Jan Rzepkowski 247777 <247777@edu.p.lodz.pl>
2024-03-27 12:21:07 +0100

    tuple comment and rename attribute

--- Fifteen Puzzle/bfs.py
+++ Fifteen Puzzle/bfs.py
@@ -8,20 +8,22 @@ class Bfs:
         self.visited_states = 1
         self.processed_states = 0
         self.elapsed_time = 0
-        self.max_recursion_depth = 0
+        self.max_recursion_reached = 0
         self.visited = set()
 
     def solve(self):
         start_time = time.time()
         queue = deque([(self.puzzle, [])])  # Kolejka przechowująca układanki i ich ścieżki
         self.visited.add(tuple(map(tuple, self.puzzle.board)))
+        # map(tuple, self.puzzle.board) mapujemy kazdy wiersz planszy na krotke - tuple
+        # tuple(map(tuple, self.puzzle.board)) zamieniamy na krotke krotek, a kazda krotka to jeden wiersz ukladanki
 
         while queue:
             current_puzzle, path = queue.popleft()  # Pobranie pierwszego elementu z kolejki
             self.processed_states += 1
 
-            if len(path) > self.max_recursion_depth:
-                self.max_recursion_depth = len(path)
+            if len(path) > self.max_recursion_reached:
+                self.max_recursion_reached = len(path)
 
             if current_puzzle.is_solved():
                 end_time = time.time()
@@ -30,7 +32,7 @@ class Bfs:
                     "path_length": len(path),
                     "visited_states": self.visited_states,
                     "processed_states": self.processed_states,
-                    "max_recursion_depth": self.max_recursion_depth,
+                    "max_recursion_reached": self.max_recursion_reached,
                     "elapsed_time": self.elapsed_time,
                     "solution": path
                 }  # Jeśli układanka jest rozwiązana, zwróć dodatkowe informacje
